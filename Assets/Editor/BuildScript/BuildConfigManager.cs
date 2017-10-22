8ecefd1c4b0a47c762ced21f1f51ecc0fd0acb82
David W <dlweatherhead@gmail.com>
2017-10-22 21:32:56 +0200

    Started work on Config Management UI

---
+++ Assets/Editor/BuildScript/BuildConfigManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class BuildConfigManager {
+
+	private static string config_dir = "/Editor/BuildScript";
+
+	private static string filename = "/config.txt";
+	private static string metafilename = "/config.txt.meta";
+
+	public string majorBuild;
+	public string minorBuild;
+
+	public void DeleteConfigFile() {
+		string absoluteConfigPath = Application.dataPath + config_dir;
+		System.IO.File.Delete (absoluteConfigPath + filename);
+		System.IO.File.Delete (absoluteConfigPath + metafilename);
+	}
+
+	public void CreateConfigFile() {
+		string absoluteConfigPath = Application.dataPath + config_dir + filename;
+		System.IO.File.WriteAllText (absoluteConfigPath, "");
+	}
+
+	void LoadConfigFile() {
+
+	}
+
+	void SaveConfigFile() {
+
+	}
+}
---
+++ Assets/Editor/BuildScript/BuildConfigWindow.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class BuildConfigWindow : EditorWindow {
+
+	private static BuildConfigWindow window;
+
+	private BuildConfigManager manager;
+
+	private static int maxWidth = 100;
+	private static int largeSpace = 20;
+	private static int smallSpace = 5;
+
+	[MenuItem("Build/Build Manager")]
+	static void Init() {
+		window = EditorWindow.GetWindow<BuildConfigWindow> ();
+		window.Initialise();
+		window.Show ();
+	}
+
+	private void Initialise() {
+		manager = new BuildConfigManager ();
+	}
+
+	void OnGUI() {
+		GUILayout.Label ("Manage Build Config", EditorStyles.boldLabel);
+
+		GUILayout.Space (smallSpace);
+		createFileManagement ();
+
+		GUILayout.Space (smallSpace);
+		createVersionNumberInput ();
+
+		GUILayout.Space (smallSpace);
+		saveConfiguration ();
+	}
+
+	private void createFileManagement() {
+		GUILayout.BeginHorizontal (); 
+		{
+			if (GUILayout.Button ("Create File", GUILayout.ExpandWidth(false))) {
+				manager.CreateConfigFile ();
+			}
+
+			GUILayout.Space (largeSpace);
+
+			if (GUILayout.Button ("Delete File", GUILayout.ExpandWidth(false))) {
+				manager.DeleteConfigFile ();
+			}
+		}
+		GUILayout.EndHorizontal ();
+	}
+
+	private void createVersionNumberInput() {
+		GUILayout.BeginHorizontal ();
+		{
+			GUILayout.Label ("Major", GUILayout.ExpandWidth(false));
+			manager.majorBuild = GUILayout.TextArea (manager.majorBuild, GUILayout.MinWidth(largeSpace), GUILayout.MaxWidth(maxWidth));
+
+			GUILayout.Space (largeSpace);
+
+			GUILayout.Label ("Minor", GUILayout.ExpandWidth(false));
+			manager.minorBuild = GUILayout.TextArea (manager.minorBuild, GUILayout.MinWidth(largeSpace), GUILayout.MaxWidth(maxWidth));
+		}
+		GUILayout.EndHorizontal ();
+
+	}
+
+	private void saveConfiguration() {
+		GUILayout.BeginHorizontal (); 
+		{
+			if (GUILayout.Button ("Save Config", GUILayout.ExpandWidth(false))) {
+				manager.CreateConfigFile ();
+			}
+		}
+		GUILayout.EndHorizontal ();
+	}
+}
